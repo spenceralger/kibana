b3e9fc9d3e76db6e30802625fcfdcfc74c984ae5
spalger <email@spalger.com>
2016-08-25 11:21:57 -0700

    re-enable and autofix prefer-const rule

--- src/ui/public/field_editor/field_editor.js
+++ src/ui/public/field_editor/field_editor.js
@@ -11,10 +11,10 @@ import fieldEditorTemplate from 'ui/field_editor/field_editor.html';
 uiModules
 .get('kibana', ['colorpicker.module'])
 .directive('fieldEditor', function (Private, $sce) {
-  let fieldFormats = Private(RegistryFieldFormatsProvider);
-  let Field = Private(IndexPatternsFieldProvider);
-  let scriptingInfo = $sce.trustAsHtml(require('ui/field_editor/scripting_info.html'));
-  let scriptingWarning = $sce.trustAsHtml(require('ui/field_editor/scripting_warning.html'));
+  const fieldFormats = Private(RegistryFieldFormatsProvider);
+  const Field = Private(IndexPatternsFieldProvider);
+  const scriptingInfo = $sce.trustAsHtml(require('ui/field_editor/scripting_info.html'));
+  const scriptingWarning = $sce.trustAsHtml(require('ui/field_editor/scripting_warning.html'));
 
   return {
     restrict: 'E',
@@ -25,8 +25,8 @@ uiModules
     },
     controllerAs: 'editor',
     controller: function ($scope, Notifier, kbnUrl) {
-      let self = this;
-      let notify = new Notifier({ location: 'Field Editor' });
+      const self = this;
+      const notify = new Notifier({ location: 'Field Editor' });
 
       self.scriptingInfo = scriptingInfo;
       self.scriptingWarning = scriptingWarning;
@@ -43,9 +43,9 @@ uiModules
 
       self.cancel = redirectAway;
       self.save = function () {
-        let indexPattern = self.indexPattern;
-        let fields = indexPattern.fields;
-        let field = self.field.toActualField();
+        const indexPattern = self.indexPattern;
+        const fields = indexPattern.fields;
+        const field = self.field.toActualField();
 
         fields.remove({ name: field.name });
         fields.push(field);
@@ -64,8 +64,8 @@ uiModules
       };
 
       self.delete = function () {
-        let indexPattern = self.indexPattern;
-        let field = self.field;
+        const indexPattern = self.indexPattern;
+        const field = self.field;
 
         indexPattern.fields.remove({ name: field.name });
         return indexPattern.save()
@@ -76,9 +76,9 @@ uiModules
       };
 
       $scope.$watch('editor.selectedFormatId', function (cur, prev) {
-        let format = self.field.format;
-        let changedFormat = cur !== prev;
-        let missingFormat = cur && (!format || format.type.id !== cur);
+        const format = self.field.format;
+        const changedFormat = cur !== prev;
+        const missingFormat = cur && (!format || format.type.id !== cur);
 
         if (!changedFormat || !missingFormat) return;
 
@@ -87,15 +87,15 @@ uiModules
       });
 
       $scope.$watch('editor.formatParams', function () {
-        let FieldFormat = getFieldFormatType();
+        const FieldFormat = getFieldFormatType();
         self.field.format = new FieldFormat(self.formatParams);
       }, true);
 
       // copy the defined properties of the field to a plain object
       // which is mutable, and capture the changed seperately.
       function shadowCopy(field) {
-        let changes = {};
-        let shadowProps = {
+        const changes = {};
+        const shadowProps = {
           toActualField: {
             // bring the shadow copy out of the shadows
             value: function toActualField() {
@@ -105,7 +105,7 @@ uiModules
         };
 
         Object.getOwnPropertyNames(field).forEach(function (prop) {
-          let desc = Object.getOwnPropertyDescriptor(field, prop);
+          const desc = Object.getOwnPropertyDescriptor(field, prop);
           shadowProps[prop] = {
             enumerable: desc.enumerable,
             get: function () {
@@ -130,7 +130,7 @@ uiModules
       }
 
       function initDefaultFormat() {
-        let def = Object.create(fieldFormats.getDefaultType(self.field.type));
+        const def = Object.create(fieldFormats.getDefaultType(self.field.type));
 
         // explicitly set to undefined to prevent inheritting the prototypes id
         def.id = undefined;
